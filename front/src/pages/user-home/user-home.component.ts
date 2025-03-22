4ac8fd851afcb2f498634483097bd3210dcfdffb
Siori9 <clara.layus@hotmail.fr>
2025-03-23 00:13:21 +0100

    fix(front): import never used error

--- front/src/pages/user-home/user-home.component.ts
+++ front/src/pages/user-home/user-home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import {FormAuthComponent} from '../../components/molecules/form-auth/form-auth.component';
 import {HomeFooterComponent} from '../../components/molecules/home-footer/home-footer.component';
 import {HomeHeaderComponent} from '../../components/molecules/home-header/home-header.component';
 
 @Component({
   selector: 'app-user-home',
   imports: [
-    FormAuthComponent,
     HomeFooterComponent,
     HomeHeaderComponent
   ],
