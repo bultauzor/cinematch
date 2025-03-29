f25b76417b0014f143f88cabc05c313e9772df2c
hcaumeil <caumeilhel@cy-tech.fr>
2025-03-29 18:44:20 +0100

    feat(front): Add working search system

---
+++ front/src/models/api.ts
@@ -0,0 +1,23 @@
+export type AuthToken = {
+  token: string;
+};
+
+export type ApiError = {
+  error: string;
+  error_code: string;
+};
+
+export type ContentType = "movie"| "show"
+
+export type Content = {
+    content_id: string,
+    provider_id: string,
+    updated_at: string,
+    content_type: ContentType,
+    title: string,
+    overview: string,
+    poster?: string,
+    release_date: string,
+    genres: string[],
+}
+
